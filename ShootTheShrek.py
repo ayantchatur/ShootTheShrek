0dfea628f9a56ead0f34e9819d0585c5fa52c181
ayantchatur <165531595+ayantchatur@users.noreply.github.com>
2025-01-30 16:20:46 -0800

    Add files via upload

--- ShootTheShrek.py
+++ ShootTheShrek.py
@@ -3,14 +3,14 @@ from random import randint
 
 TITLE="Shoot The Shrek"
 
-WIDTH=500
-HEIGHT=500
+WIDTH=750
+HEIGHT=750
 message="Click Shrek Potter to shoot him"
 shrek = Actor('shrekpotter')
 
 def draw():
     screen.clear()
-    screen.fill(color=(100,28,30))
+    screen.fill(color=(175,60,50))
     shrek.draw()
     screen.draw.text(message, center=(200,10),fontsize=30)
 
@@ -22,9 +22,11 @@ def on_mouse_down(pos):
     global message
     if shrek.collidepoint(pos):
         message ="Good shot"
+        sounds.good.play()
         place_shrek()
     else:
         message="You missed lol"
+        sounds.bad.play()
 
 place_shrek()
 pgzrun.go()
